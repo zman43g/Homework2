e012ab1c1854be73503e15769e8a9f7b00216821
zman43g <zman3@yandex.ru>
2025-08-31 12:10:31 +0700

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 25000;
+        byte b = 100;
+        short c = -1000;
+        long d = 188651;
+        float f = -56.52f;
+        double bigNum = 98721.999201;
+
+System.out.println("Значение переменной a с типом int равно " + a);
+System.out.println("Значение переменной b с типом byte равно " + b);
+System.out.println("Значение переменной c с типом short равно " + c);
+System.out.println("Значение переменной d с типом long равно " + d);
+System.out.println("Значение переменной f с типом float равно " + f);
+System.out.println("Значение переменной bigNum с типом double равно " + bigNum);
+    }
+}
